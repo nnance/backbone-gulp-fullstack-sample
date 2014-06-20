c4799870468c1cfd4884b0d71b2ffa85561cd273
Nick Nance <nick.nance@connectsolutions.com>
2014-06-20 12:28:32 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,129 @@
+'use strict';
+// generated on 2014-06-20 using generator-backbone-fullstack 0.0.0
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var rimraf = require('rimraf');
+
+gulp.task('styles', function () {
+    return gulp.src('app/styles/main.scss')
+        .pipe($.rubySass({
+            style: 'expanded',
+            precision: 10
+        }))
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('.tmp/styles'));
+});
+
+gulp.task('jshint', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('html', ['styles'], function () {
+    var lazypipe = require('lazypipe');
+    var cssChannel = lazypipe()
+      .pipe($.csso)
+      .pipe($.replace, 'bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap','fonts');
+
+    return gulp.src('app/*.html')
+        .pipe($.useref.assets({searchPath: '{.tmp,app}'}))
+        .pipe($.if('*.js', $.uglify()))
+        .pipe($.if('*.css', cssChannel()))
+        .pipe($.useref.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function () {
+    var streamqueue = require('streamqueue');
+    return streamqueue({objectMode: true},
+            $.bowerFiles(),
+            gulp.src('app/fonts/**/*')
+        )
+        .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+        .pipe($.flatten())
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('extras', function () {
+    return gulp.src(['app/*.*', '!app/*.html'], {dot: true})
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function (cb) {
+    rimraf('.tmp', function () {
+        rimraf('dist', cb);
+    });
+});
+
+gulp.task('connect', function () {
+    var connect = require('connect');
+    var app = connect()
+        .use(require('connect-livereload')({port: 35729}))
+        .use(connect.static('app'))
+        .use(connect.static('.tmp'))
+        // paths to bower_components should be relative to the current file
+        // e.g. in app/index.html you should use ../bower_components
+        .use('/bower_components', connect.static('bower_components'))
+        .use(connect.directory('app'));
+
+    require('http').createServer(app)
+        .listen(9000)
+        .on('listening', function () {
+            console.log('Started connect web server on http://localhost:9000');
+        });
+});
+
+gulp.task('serve', ['connect', 'styles'], function () {
+    require('opn')('http://localhost:9000');
+});
+
+// inject bower components
+gulp.task('wiredep', function () {
+    var wiredep = require('wiredep').stream;
+
+    gulp.src('app/styles/*.scss')
+        .pipe(wiredep({directory: 'bower_components'}))
+        .pipe(gulp.dest('app/styles'));
+
+    gulp.src('app/*.html')
+        .pipe(wiredep({
+            directory: 'bower_components',
+            exclude: ['bootstrap-sass-official']
+        }))
+        .pipe(gulp.dest('app'));
+});
+
+gulp.task('watch', ['connect', 'serve'], function () {
+    $.livereload.listen();
+
+    // watch for changes
+    gulp.watch([
+        'app/*.html',
+        '.tmp/styles/**/*.css',
+        'app/scripts/**/*.js',
+        'app/images/**/*'
+    ]).on('change', $.livereload.changed);
+
+    gulp.watch('app/styles/**/*.scss', ['styles']);
+    gulp.watch('bower.json', ['wiredep']);
+});
+
+gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras'], function () {
+    return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
